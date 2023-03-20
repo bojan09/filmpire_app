872bd13a06a5792ffb6e053a35f6969fa1ed8866
Bojan <bojan-stanimirovski097@hotmail.com>
2023-03-20 20:39:20 +0100

    added genreSearch and category with AlanAI

--- src/components/AlanAI/Alan.jsx
+++ src/components/AlanAI/Alan.jsx
@@ -1,5 +1,16 @@
 import React, { useEffect, useContext } from "react";
 
+// redux
+import { useDispatch } from "react-redux";
+
+// react dom
+import { useHistory } from "react-router-dom";
+
+import {
+  selectGenreOrCategory,
+  searchMovie,
+} from "../../features/currentGenreOrCategory";
+
 // change color functionality
 import { ColorModeContext } from "../../utils/ToggleColorMode";
 
@@ -9,25 +20,46 @@ import { fetchToken } from "../../utils";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
 const useAlan = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
   const { setMode } = useContext(ColorModeContext);
 
   useEffect(() => {
     alanBtn({
       key: "2b1895da36ff0dd996d6432757a753d32e956eca572e1d8b807a3e2338fdd0dc/stage",
-      onCommand: ({ command, mode }) => {
-        if (command === "changeMode") {
-          // Call the client code that will react to the received command
+      onCommand: ({ command, mode, genres, genreOrCategory }) => {
+        // SEARCH GENRES
+        if (command === "chooseGenre") {
+          const foundGenre = genres.find(
+            (g) => g.name.toLowerCase() === genreOrCategory.toLowerCase()
+          );
+          if (foundGenre) {
+            history.push("/");
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else if (genreOrCategory) {
+            // top rated | popular | upcoming
+            const category = genreOrCategory.startsWith("top")
+              ? "top_rated"
+              : genreOrCategory;
+            history.push("/");
+            dispatch(selectGenreOrCategory(category));
+          }
+
+          // LIGHT / DARK MODE
+        } else if (command === "changeMode") {
           if (mode === "light") {
             setMode("light");
           } else {
             setMode("dark");
           }
+
+          // LOG IN / LOG OUT
         } else if (command === "login") {
           fetchToken();
         } else if (command === "logout") {
           localStorage.clear();
-
-          window.location.href = "/";
+          history.push("/");
         }
       },
     });
